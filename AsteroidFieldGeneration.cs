abc5db7c28e3c7d8bdc22171c59d45da26f5b1c3
pkmango <pk-man@yandex.ru>
2020-04-16 21:29:09 +0300

    Added AsteroidFieldGeneration

---
+++ AsteroidFieldGeneration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidFieldGeneration : MonoBehaviour
+{
+    public GameObject[] asteroidsBG; //Массив с астероидами для фона
+    public Vector2 size; //Диапазон размеров астероидов
+    public Vector2 spawnInterval; //Диапазон интервалов появления нового астероида
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
