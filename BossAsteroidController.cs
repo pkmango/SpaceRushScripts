ecedaaf1551bc3663cbefedd34d9366c645432d0
pkmango <pk-man@yandex.ru>
2020-04-22 11:28:33 +0300

    Added TransparencyChange. This feature adds the ability to change the transparency of the material for a set time.

---
+++ BossAsteroidController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAsteroidController : MonoBehaviour
+{
+    public float startWait;
+    public float shakeTime;
+    public float shakeRate;
+    public float firstManeuverTime;
+    public float maneuverRate;
+    public float maneuverTime;
+    public float maneuverPosition1;
+    public float maneuverPosition2;
+    public GameObject energyShield;
+
+
+    void Start()
+    {
+        
+        StartCoroutine(AddingShield());
+    }
+
+    IEnumerator AddingShield()
+    {
+        yield return new WaitForSeconds(startWait);
+        Instantiate(energyShield, transform.position, Quaternion.identity);
+        
+    }
+
+
+}
---
+++ TransparencyChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparencyChange : MonoBehaviour
+{
+    public float changeTime;
+
+    private Color myColor = Color.white;
+
+    void Start()
+    {
+        StartCoroutine(SmoothChange(0f, 1f, changeTime));
+    }
+
+    IEnumerator SmoothChange(float from, float to, float timer)
+    {
+
+        float t = 0.0f;
+
+        myColor.a = from;
+
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime / timer;
+
+            myColor.a = Mathf.Lerp(from, to, t);
+            gameObject.GetComponent<Renderer>().material.color = myColor;
+
+            yield return null;
+        }
+
+
+    }
+
+}
